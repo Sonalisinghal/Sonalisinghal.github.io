63140b672a2b95d321f8e0b3cc201dd1e4f44e2b
Sonali <sonali.singhal1999@gmail.com>
2020-04-17 02:02:30 +0530

    Blogs

---
+++ generatePages/blogs.py
@@ -0,0 +1,40 @@
+datasets=[{"Title":"Breaking down barriers to open source contributions",
+"Author":"Tanea Agrawaal",
+"Date":"Mar 24,2020",
+"Description":"Tanea helps clearning some doubts about RGSoC and how it can help one to get started with FOSS",
+"ImageName":"pic02.jpg",
+"Link":"https://railsgirlssummerofcode.org/blog/2020-03-24-breaking-barriers"
+},
+{
+"Title":"Women Tech Makers Experience 2019",
+"Author":"Shreya Gupta",
+"Date":"Mar 17, 2020",
+"Description":"Shreya explains the application process and an insight of what goes on in the Women Tech Maker retreat",
+"ImageName":"pic04.jpg",
+"Link":"https://medium.com/@shreyagupta0806/google-women-techmakers-experience-2019-a9b1d0e19877"
+},
+{
+"Title":"Short guide to starting competitive programming",
+"Author":"Lavina Jain",
+"Date":"Dec 2, 2019",
+"Description":"Lavina gives some suggestions on how one can get started with competitive programming and how to practice coding.",
+"ImageName":"pic03.jpg",
+"Link":"https://medium.com/@lavina16052/short-guide-to-starting-competitive-programming-ff60dfdfb90b"
+},
+{
+"Title":"Google Step Internship",
+"Author":"Lavina Jain",
+"Date":"Sept 22, 2019",
+"Description":"In this blog, Lavina explains the interview process in basic: getting an interview call, the timeline, the course and the structure of the interviews.",
+"ImageName":"pic05.jpg",
+"Link":"https://medium.com/@lavina16052/google-step-internship-5b5141e0cac6"
+},
+{
+"Title":"Google Venkat Scholarship - A Dream Come True",
+"Author":"Baani Leen Kaur",
+"Date":"Aug 14, 2019",
+"Description":"Baani narrates her experience of how she got the prestigious Google Venkat Scholarship and the retreat",
+"ImageName":"pic06.jpg",
+"Link":"https://medium.com/@BaaniLeen/google-venkat-scholarship-a-dream-come-true-c54f5b42ac1d"
+}
+]
---
+++ generatePages/generateBlogs.py
@@ -0,0 +1,52 @@
+import os
+from blogs import datasets
+
+startComment = "START AUTO-INSERT DATASETS"
+endComment = "END AUTO-INSERT DATASETS"
+pageToChange = "../blogs.html"
+tempfile = 'blogs.temp'
+
+def updatePage():
+  with open(pageToChange,'r') as file:
+    orgLines = file.readlines()
+
+  with open(pageToChange,'w') as file:
+    write = True
+    for x in orgLines:
+      if endComment in x:
+        write = True
+        file.write('\n')
+      if write:
+        file.write(x)
+      if startComment in x:
+        with open(tempfile,'r') as temp:
+          add = temp.read()
+        file.write(add)
+        write = False
+
+with open(tempfile, 'w') as f:
+  for ds in datasets:
+    print("Adding dataset for "+ds["Title"])
+    f.write("\n")
+    f.write('''
+                            <article>
+                              <div class="wrapper">
+                                <div class="blog">
+                                  <div class="blog__image">
+                                    <img src="images/'''+ds["ImageName"]+'''" alt="" />
+                                  </div>
+                                  <div class="blog__author">'''+ds["Author"]+" | "+ds["Date"]+'''</div>
+                                  <div class="blog__unit-name">'''+ds["Title"]+'''</div>
+                                  <div class="blog__unit-description">
+                                    '''+ds["Description"]+'''
+                                  </div>
+                                  <a href="'''+ds["Link"]+'''" class="button purple small">Read more</a>
+                              </div> <!-- end wrapper -->
+                          </article>
+              ''')
+updatePage()
+
+#remove <tempfile> as we don't need it anymore
+os.remove(tempfile)
+
+                
