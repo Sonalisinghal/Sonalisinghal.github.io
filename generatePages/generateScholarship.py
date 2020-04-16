30d55c80be5da42c7b028aae81a66a3a1b085d27
Sonali <sonali.singhal1999@gmail.com>
2020-04-16 17:57:25 +0530

    Scholarships

--- generatePages/generateScholarship.py
+++ generatePages/generateScholarship.py
@@ -3,7 +3,7 @@ from scholarship import datasets
 
 startComment = "START AUTO-INSERT DATASETS"
 endComment = "END AUTO-INSERT DATASETS"
-pageToChange = "../scholarship.html"
+pageToChange = "../scholarships.html"
 tempfile = 'scholarship.temp'
 
 def updatePage():
@@ -26,24 +26,24 @@ def updatePage():
 
 with open(tempfile, 'w') as f:
   for ds in datasets:
-    print("Adding dataset for "+ds["Contest"])
+    print("Adding dataset for "+ds["Scholarship"])
     f.write("\n")
     f.write('''
                 <div class="spotlight">
                   <div class="content">
                     <header class="major">
-                      <h3>'''+ds["Contest"]+'''</h3>
+                      <h3>'''+ds["Scholarship"]+'''</h3>
                     </header>
                     <p>'''+ds['About']+'''</p>
-                    <h6><b>Applications Open: </b>'''+ds['Applications Open']+'''</h6>
-                    <h6><b>Applications Close: </b>'''+ds["Last Date to Apply"]+'''</h6>
+                    <h6><b>Applications Open: </b>'''+ds['Applications open']+'''</h6>
+                    <h6><b>Applications Close: </b>'''+ds["Application Close"]+'''</h6>
                     <h6><b>Eligibiblity: </b>'''+ds['Eligibility']+'''</h6>
-                    <h6><b>Benefits: </b>'''+ds['Stipend']+'''</h6>
+                    <h6><b>Benefits: </b>'''+ds['Benefits']+'''</h6>
                     <ul class="actions">
                       <li><a href="'''+ds['Link']+'''" class="button">Learn More</a></li>
                     </ul>
                   </div>
-                  <span class="image"><img src="images/'''+ds['Imagename']+'''" alt="" /></span>
+                  <span class="image"><img src="images/'''+ds['ImageName']+'''" alt="" /></span>
                 </div>
                 <hr>''')
 updatePage()
--- generatePages/scholarship.py
+++ generatePages/scholarship.py
@@ -1,32 +1,13 @@
 datasets=[
-        {
-            "Scholarship": "Grace hopper Scholarship",
-            "Applications open": "January-March",
-            "Link": "https://ghc.anitab.org/2020-student-academic/scholarships/"
-        },
-        {
-            "Scholarship": "Pass it on awards",
-            "Applications open": "January-March",
-            "Link": "https://systers.submittable.com/submit/76301/systers-pass-it-on-awards"
-        },
-        {
-            "Scholarship": "Adobe Women In tech Scholarship",
-            "Eligibility": "Be an Indian female citizen student\nMust be enrolled as a full time student in a formal technology 4 year BE / B.Tech education program or an Integrated ME/MS/MTech program at an Indian University or Institute at the time of applying and should be completing the program in Academic year 2020-21.\nShould be pursuing a Major or Minor in the following streams: Computer Science/Engineering, Information Science, Data Science, Electrical/Electronics Engineering, Math and Computing\nMay apply to ONLY ONE of the following two Scholarships – Adobe India Women-In-Technology Scholarship OR the Adobe Global Women In Technology Scholarship.\nAnyone who is a “related party” to an Adobe employee is not eligible to apply.\nDefinition of Related Party – Any person having the following relationship to an Adobe employee: spouse, domestic partner, child, parent, sibling, child-in-law, parent-in-law, or sibling-in-law; any other relative or person who lives in the household of an Adobe employee.",
-            "Applications open": "August-September"
-        },
-        {
-            "Scholarship": "Google's Venkat Panchapakesan Memorial Scholarship"
-        },
-        {
-            "Scholarship": "Red Hat",
-            "About": "Women in Open source"
-        },
-        {
-            "Scholarship": "Women tech Makers"
-        },
-        {
-            "Scholarship": "Western Digital Scholarship Program"
-        }
+        {"Scholarship":"AnitaB.org Grace hopper Scholarship","About":"The GHC Scholars Program provides funds for women who are either undergraduate students, graduate students, or faculty to attend Grace Hopper Celebration.","Eligibility":"For students: Women, 18+ by September of that year. Full-time student enrolled in an accredited degree program at a college or university pursuing Computer Science/Engineering or related fields.","Applications open":"January","Application Close":"March","Benefits":"Scholarship covers the cost of individual registeration for the GHC, hotel accomodation, airfare and stipend (to be used for travel, meals, and incidentals such as rideshares, baggage fees, etc.)","ImageName":"ghc.jpg","Link":"https://ghc.anitab.org/2020-student-academic/scholarships/"},
+        {"Scholarship":"AnitaB.org Pass it on awards","About":"The cash award helps fund women in computing or projects that inspire and support girls and women to enter computing. Recipients are encouraged to \"pass on\" the benefits they gain from the award, creating a movement of women helping women.","Eligibility":"Any woman over 18 may apply, though she must be in or aspiring to be in computing fields","Applications open":"January","Application Close":"March","Benefits":"Awards range up to $1,000 USD","ImageName":"passiton.jpg","Link":"https://systers.submittable.com/submit/76301/systers-pass-it-on-awards"},
+        {"Scholarship":"Adobe Women In tech Scholarship","About":"Aim towards creating gender equality in science, technology and engineering domains by encouraging women to showcase their excellence in computing and technology and become future leaders and role models in the field.","Eligibility":"Be an Indian female citizen student,enrolled as a full time student in a formal technology 4 year BE / B.Tech education program or an Integrated ME/MS/MTech program at an Indian University, completing the program next year.\nShould be pursuing a Major or Minor in Computer Science/Engineering or related fields.","Applications open":"August","Application Close":"September","Benefits":"Tution fee for remainder of recipient's education, Opportunity for summer internship at Adobe India, Mentoring by a senior technology leader from Adobe, Travel to Grace Hopper Conference India, including participation fees.","ImageName":"adobe.jpg","Link":"https://research.adobe.com/adobe-india-women-in-technology-scholarship/"},
+        {"Scholarship":"Venkat Panchapakesan Memorial Scholarship by Google","About":"Scholarship for people who are passionate to leverage the field of computer science to transform systems","Eligibility":"Indian, Full time student pursuing Computer Science/Engineering or related fields.","Applications open":"August","Application Close":"September","Benefits":"750 USD (must be spent on tuition, fees, books, supplies and equipment required for the students' classes at their primary university)","ImageName":"google.png","Link":"https://buildyourfuture.withgoogle.com/scholarships/venkat-panchapakesan-memorial-scholarship/"},
+        {"Scholarship":"Red Hat","About":"These awards are given to honor women who make important contributions to open source projects and communities, or those making innovative use of open source methodology.","Eligibility":"Women","Applications open":"January","Application Close":"January","Benefits":"$2,500 stipend, Paid registration, flight, and hotel accommodations for Red Hat Summit","ImageName":"redhat.jpg","Link":"https://www.redhat.com/en/about/women-in-open-source"},
+        {"Scholarship":"Google Women tech Makers","About":"An academic scholarship, awarded based on academic performance, leadership, and impact on the community of women in tech.","Eligibility":"Women, in year 1 or year 2 of their undergraduate program","Applications open":"March","Application Close":"April","Benefits":"Stipend and invitation to attend Women Techmakers Scholars' Retreat","ImageName":"wtm.jpg","Link":"https://www.womentechmakers.com/scholars"},
+        {"Scholarship":"Western Digital Scholarship Program","About":"Aim to encourage STEM education for underrepresented and underprivileged students","Eligibility":"Undergraduate student enrolled (or to be enrolled) on a full-time basis at an accredited four-year college or university in the United States, China, Japan, India, Malaysia, Philippines or Thailand (pursuing Computer Science, Engineering, Mathematics or Science)","Applications open":"January","Application Close":"April","Benefits":"Upto $5000","ImageName":"westerndigital.jpg","Link":"https://www.westerndigital.com/company/corporate-philanthropy/scholarship-programs"},
+        {"Scholarship":"Nutanix .heart WIT Scholarship","About":"Scholarships to help young women develop the skills they need to pursue their passion of computer science.","Eligibility":"Female students in the United States, India, UK and Serbia in Final year of undergraduate degree or higher degree pursuing Computer Science or related field","Applications open":"November","Application Close":"May","Benefits":"Upto $2500 in India and Siberia, Upto $7500 in USA and UK","ImageName":"nutanix.jpg","Link":"https://www.nutanix.com/scholarships"},
+        {"Scholarship":"Adobe Research Women in Tech Scholarship","About":"This scholarship recognizes outstanding undergraduate and masters female students anywhere in the world who are studying computer science.","Eligibility":"Female student currently enrolled as an undergraduate (2nd year or above) or masters student at a university pursuing Computer Science or related field.","Applications open":"August","Application Close":"September","Benefits":"$10000, Creative cloud Membership, Internship opportunity","ImageName":"adoberesearchscholarship.jpg","Link":"https://research.adobe.com/scholarship/"}
     ]
 
 
