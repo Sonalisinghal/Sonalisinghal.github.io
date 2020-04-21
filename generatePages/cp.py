14d16af064248dda14adc5db91cc429d1f308341
Sonali <sonali.singhal1999@gmail.com>
2020-04-21 12:36:54 +0530

    every week

---
+++ generatePages/cp.py
@@ -0,0 +1,7 @@
+datasets=[
+        {"cp":"ACM-ICPC","About":"It is the oldest, largest, and most prestigious programming contest in the world.","Eligibility":"A student must be enrolled in a degree program at the team’s institution with at least a half-time load.","Applications open":"September-October","Team Size":"3","ImageName":"icpc.jpg","Link":"https://icpc.baylor.edu/"},
+        {"cp":"","About":"","Eligibility":"","Applications open":"","Team Size":"","ImageName":"","Link":""},
+        {"cp":"","About":"","Eligibility":"","Applications open":"","Team Size":"","ImageName":"","Link":""},
+        {"cp":"","About":"","Eligibility":"","Applications open":"","Team Size":"","ImageName":"","Link":""},
+        {"cp":"","About":"","Eligibility":"","Applications open":"","Team Size":"","ImageName":"","Link":""},
+        ]
---
+++ generatePages/generateCP.py
@@ -0,0 +1,51 @@
+import os
+from cp import datasets
+
+startComment = "START AUTO-INSERT DATASETS"
+endComment = "END AUTO-INSERT DATASETS"
+pageToChange = "../competitiveprogramming.html"
+tempfile = 'cp.temp'
+
+def updatePage():
+  with open(pageToChange,'r') as file:
+    orgLines = file.readlines()
+
+  with open(pageToChange,'w') as file:
+    write = True
+    for x in orgLines:
+      if endComment in x:
+        write = True
+        file.write('\n')
+      if write:
+        file.write(x)
+      if startComment in x:
+        with open(tempfile,'r') as temp:
+          add = temp.read()
+        file.write(add)
+        write = False
+
+with open(tempfile, 'w') as f:
+  for ds in datasets:
+    print("Adding dataset for "+ds["cp"])
+    f.write("\n")
+    f.write('''
+                <div class="spotlight">
+                  <div class="content">
+                    <h2>'''ds["cp"]'''</h2>
+                    <p>'''ds["About"]'''</p>
+                    <h6><b>Applications Open: </b>'''ds["Applications open"]'''</h6>
+                    <h6><b>Team Size: </b>'''ds["Team Size"]'''</h6>
+                    <h6><b>Eligibiblity: </b>'''ds["Eligibiblity"]'''</h6>
+                    <ul class="actions">
+                      <li><a target="_blank" href="'''ds["Link"]'''" class="button">Learn More</a></li>
+                    </ul>
+                  </div>
+                  <span class="image"><img src="images/'''ds["ImageName"]'''" alt="" /></span>
+                </div>
+                <hr>''')
+updatePage()
+
+#remove <tempfile> as we don't need it anymore
+os.remove(tempfile)
+
+                
